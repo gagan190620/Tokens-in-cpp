75ade1e2e782e93c207bf0a06d4affb26e0fde62
gagan190620 <85725628+gagan190620@users.noreply.github.com>
2021-07-06 10:28:51 +0530

    sizeof.cpp
    program contains size of data types

---
+++ sizeof.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    double y;
+    char z;
+    float a;
+    cout<<"Size of integer is: "<<sizeof(x)<<" bytes";
+    cout<<"\n********************************\n";
+    cout<<"Size of double is:"<<sizeof(y)<<" bytes";
+    cout<<"\n********************************\n";
+    cout<<"Size of char is:"<<sizeof(z)<<" bytes";
+    cout<<"\n********************************\n";
+    cout<<"Size of float is:"<<sizeof(a)<<" bytes";
+   
+    return 0;
+}
