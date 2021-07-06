773820228e3aaf4f2048b81a59383f40b1ff4306
gagan190620 <85725628+gagan190620@users.noreply.github.com>
2021-07-06 10:17:32 +0530

    Product_of_numbers.cpp
    File contains program in cpp for finding product of two numbers

---
+++ Product_of_number.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double x,y;
+    cout<<"Please enter two numbers:\n";
+    cout<<"********************************\n";
+    cout<<"Enter first number\n";
+    cin>>x;
+    cout<<"\nEnter second number\n";
+    cin>>y;
+    cout<<"Product of entered two numbers is:\n"<<(x*y);
+
+    return 0;
+}
